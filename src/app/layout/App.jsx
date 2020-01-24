7758c643188919947f13b59f950669c39f106959
Ravi Goswami <ravigoswami@cdnsol.com>
2020-01-24 09:15:20 +0530

    made design of header with sementic Ui

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,15 @@
 import React,{Component} from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nev/NevBar/NavBar';
+import '../../../src/index.css'
 class App extends Component{
  render(){
   return (
     <div>
         <h1>re-vents</h1>
-       </div>
+        <NavBar/>
+        <EventDashboard/>
+    </div>
      )
  }
   
