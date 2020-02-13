265203ce070568542acb26782d47883ae34a20d6
Ravi Goswami <ravigoswami@cdnsol.com>
2020-02-13 16:58:12 +0530

    form hide show and data render.

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -5,9 +5,12 @@ class EventList extends Component {
     render() {
         return (
           <Fragment>
-               <EventListItem/>
-               <EventListItem/>
-               <EventListItem/>
+            {
+              this.props.events.map(event => 
+                 <EventListItem key={event.id} event = {event} />
+              )
+            }
+              
           </Fragment>  
         )
     }
--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Image, ListItem } from 'semantic-ui-react'
 
  class EventListAttendee extends Component {
     render() {
+        const {attendee} = this.props
         return (
-            <div>
-                
-            </div>
+            <ListItem>
+                    <Image as='a' size='mini' circular src={attendee.photoURL}/>   
+            </ListItem>
         )
     }
 }
